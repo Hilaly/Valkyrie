5e1428ae569b7e3fa2903f8001c57180004cc6a0
Igor Panchenko <i.panchenko@azurgames.com>
2022-12-18 17:44:00 +0300

    auto get rigidbody

--- Src/Valkyrie/Runtime/Playground/Features/PhysicMovementComponent.cs
+++ Src/Valkyrie/Runtime/Playground/Features/PhysicMovementComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Valkyrie.Playground.Features
@@ -5,6 +6,12 @@ namespace Valkyrie.Playground.Features
     class PhysicMovementComponent : MonoComponent, IPhysicBasedMovementComponent
     {
         [SerializeField] private Rigidbody rigidbody;
+
+        private void Awake()
+        {
+            if(rigidbody == null)
+                rigidbody = GetComponent<Rigidbody>();
+        }
 
         public Rigidbody Physic => rigidbody;
     }
